482ddf297fa16d2d84e8c57f4af878696ba290a1
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-11-25 14:30:23 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+# Telegram bot token
+TOKEN = "8570774647:AAHvjeKSY0x7Y5vTPqec1EMAySTtRBqzP3w"
+
+# Regos integration base URL
+REGOS_BASE_URL = "https://integration.regos.uz/gateway/out/088cbfefc5a84202b180e4bac8ccdf79"
+
+# Regos endpointlar
+DOC_WHOLESALE_GET_URL = f"{REGOS_BASE_URL}/v1/DocWholeSale/Get"
+WHOLESALE_OPERATION_GET_URL = f"{REGOS_BASE_URL}/v1/WholeSaleOperation/Get"
+
+# Qarz API (hozircha ishlatmaymiz)
+PARTNER_DEBT_API_URL = None
+
+# 🔥 ASOSIY GURUH — sizning xodimlar guruhi
+DEFAULT_GROUP_ID = -5079260527
