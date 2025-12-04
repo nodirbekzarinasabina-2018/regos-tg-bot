dbddcbb700a9c612021c07ec1160cde0c178adad
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-04 17:56:33 +0500

    Create bot_manager.py

---
+++ app/core/bot_manager.py
@@ -0,0 +1,15 @@
+from aiogram import Bot
+from config.settings import settings
+
+_bots: dict[str, Bot] = {}
+
+
+def get_bot(account_code: str) -> Bot | None:
+    if account_code not in settings.accounts:
+        return None
+
+    if account_code not in _bots:
+        token = settings.accounts[account_code].telegram_token
+        _bots[account_code] = Bot(token=token)
+
+    return _bots[account_code]
