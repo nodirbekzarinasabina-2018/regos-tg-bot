3d860b6473f5f224a349afb11e9a8cac5f5dcb24
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-04 17:16:40 +0500

    Create storage.py

---
+++ app/storage.py
@@ -0,0 +1,11 @@
+# account_code -> bot_token
+ACCOUNTS = {
+    # vaqtincha test uchun
+    # "test": "BOT_TOKEN_BU_YERGA",
+}
+
+# account_code -> group chat ids
+GROUPS = {}
+
+# account_code -> phone -> user_id
+USERS = {}
