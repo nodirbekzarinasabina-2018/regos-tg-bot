c8363888060168d9f04ab4100751f1e47b904d16
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-04 20:41:52 +0500

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,23 +1,12 @@
 from pydantic import BaseModel
-from typing import Dict
-
-
-class AccountSettings(BaseModel):
-    telegram_token: str
-    regos_webhook_secret: str | None = None
 
 
 class Settings(BaseModel):
-    accounts: Dict[str, AccountSettings] = {}
+    # keyinchalik qo‘shamiz:
+    # admin_username: str
+    # admin_password: str
+    # secret_key: str
+    pass
 
 
-# ⚠️ HOZIRCHA Vaqtincha
-# Keyin buni ENV + DB + Admin panelga o‘tkazamiz
-settings = Settings(
-    accounts={
-        "test": AccountSettings(
-            telegram_token="8594792866:AAGVsoyLQH0SX3Wz7EQvV54Eqg7GwlOLXec",
-            regos_webhook_secret="test-secret"
-        )
-    }
-)
+settings = Settings()
