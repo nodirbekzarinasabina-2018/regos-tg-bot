76a9c81dcab9d8821bb728dce7b3fb7ab5156938
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-05 16:04:14 +0500

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,13 +1,13 @@
 from pydantic_settings import BaseSettings
-
+from pydantic import ConfigDict
 
 class Settings(BaseSettings):
     bot1_token: str
     bot2_token: str
-    regos_url: str   # ✅ KICHIK HARF (MUHIM!)
 
-    class Config:
-        env_file = ".env"
-
+    model_config = ConfigDict(
+        env_file=".env",
+        extra="ignore"   # 🔥 MUHIM
+    )
 
 settings = Settings()
