9b9a919ac67a41a1732990728564b62118e4341e
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-05 15:58:15 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,11 @@
 from fastapi import FastAPI
-
 from app.routers import regos
 from app.core.db import init_all
-from app.core.bot_manager import start_bot, stop_all_bots
 
-app = FastAPI(title="Regos Telegram Bots")
-
+app = FastAPI(title="Regos API")
 
 @app.on_event("startup")
-async def startup():
+def startup():
     init_all()
-    await start_bot("bot1")
-    await start_bot("bot2")
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await stop_all_bots()
-
 
 app.include_router(regos.router)
