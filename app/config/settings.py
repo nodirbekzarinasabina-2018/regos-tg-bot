75243ebd6401147dfc1b17876fd18f4a2e91257e
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-05 14:23:46 +0500

    Update settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     BOT1_TOKEN: str
     BOT2_TOKEN: str
-
+    REGOS_URL: str
     class Config:
         env_file = ".env"
 
