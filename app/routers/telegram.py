efa35b6cb6aace71e86ca4215655103d615ff6e7
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-05 16:48:58 +0500

    Delete app directory

--- app/routers/telegram.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Dispatcher, types
-from app.handlers.telegram import register_group
-
-
-def setup(dp: Dispatcher, account_code: str):
-    @dp.message_handler(commands=["start"])
-    async def start_handler(message: types.Message):
-        await register_group(message, account_code)
