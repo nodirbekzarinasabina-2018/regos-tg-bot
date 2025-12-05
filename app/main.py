dc561762ec1c68445651f7126cae4125f24a2d10
nodirbekzarinasabina-2018 <nodirbekzarinasabina@gmail.com>
2025-12-05 14:04:16 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
+import asyncio
 
 from app.routers import regos
 from app.core.db import init_all
+from app.core.dispatcher import start_polling
 
 app = FastAPI(title="Regos Telegram Bots")
 
-# ✅ APP STARTIDA DB’LARNI OCHIB OLAMIZ
 @app.on_event("startup")
 async def on_startup():
     init_all()
+    asyncio.create_task(start_polling())
 
-
-# ✅ ROUTERLAR
 app.include_router(regos.router)
